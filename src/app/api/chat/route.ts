f1c9499b9994c7ec06dc5d576920834d1b5abd74
Adhayan Sharma <96379455+adhayan002@users.noreply.github.com>
2024-06-25 16:42:50 +0530

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,18 +5,18 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
 
 const generationConfig = {
-    stopSequences: ["red"],
-    maxOutputTokens: 500,
-    temperature: 0.7,
-    topP: 0.6,
-    topK: 16,
-  };
-const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig});
+  stopSequences: ["red"],
+  maxOutputTokens: 500,
+  temperature: 1,
+  topP: 0.6,
+  topK: 16,
+};
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig });
 
 export async function POST(request: NextRequest) {
 	const {messages} = await request.json();
 	const prompt = messages[messages.length - 1].content;
 	
-  const result = await model.generateContent(prompt);
-	return NextResponse.json(result.response.text() , { status: 200 });
+ const result = await model.generateContent(prompt);
+	return NextResponse.json(result.response.text().replace(/\n/g, ' ').replace(/\*/g, ' ').replace(/\\/g, '') , { status: 200 });
 }
